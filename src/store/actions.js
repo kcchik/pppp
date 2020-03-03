9627eb46e2e643cd204a076591f722effbf5966f
Kevin Chik <kevin.c.chik@gmail.com>
2020-03-03 10:40:07 -0500

    lmao

--- src/store/actions.js
+++ src/store/actions.js
@@ -6,12 +6,12 @@ export default {
     commit('setShop', 0);
   },
   async setBases({ commit, state }) {
-    let res = await fetch(`stores/${state.shops.selected}/bases`);
+    let res = await fetch(`stores/${state.shops.selected + 1}/bases`);
     res = await res.json();
     commit('setBases', res);
   },
   async setToppings({ commit, state }) {
-    let res = await fetch(`stores/${state.shops.selected}/toppings`);
+    let res = await fetch(`stores/${state.shops.selected + 1}/toppings`);
     res = await res.json();
     commit('setToppings', res);
   },
